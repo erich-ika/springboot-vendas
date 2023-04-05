50338608c72911fe9091550060009ce63b12e4fc
erich-ika <erich.sabe.tudo@gmail.com>
2023-04-05 12:19:05 -0300

    Obtendo os detalhes de um pedido
    basicamente uma nova rota toda complicada.

--- src/main/java/io/github/erichika/rest/controller/PedidoController.java
+++ src/main/java/io/github/erichika/rest/controller/PedidoController.java
@@ -1,9 +1,19 @@
 package io.github.erichika.rest.controller;
 
+import io.github.erichika.domain.entity.ItemPedido;
 import io.github.erichika.domain.entity.Pedido;
+import io.github.erichika.rest.dto.InformacoesItemPedidoDTO;
+import io.github.erichika.rest.dto.InformacoesPedidoDTO;
 import io.github.erichika.rest.dto.PedidoDTO;
 import io.github.erichika.service.PedidoService;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -22,5 +32,37 @@ public class PedidoController {
     public Integer save(@RequestBody PedidoDTO dto) {
         Pedido pedido = service.salvar(dto);
         return pedido.getId();
+    }
+
+    @GetMapping("/{id}")
+    public InformacoesPedidoDTO getById(@PathVariable Integer id) {
+        return service
+                .obterPedidoCompleto(id)
+                .map(this::converter)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Pedido não encontrado."));
+    }
+
+    private InformacoesPedidoDTO converter(Pedido pedido) {
+        return InformacoesPedidoDTO.builder()
+                .codigo(pedido.getId())
+                .dataPedido(pedido.getDataPedido().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                .cpf(pedido.getCliente().getCpf())
+                .nomeCliente(pedido.getCliente().getNome())
+                .total(pedido.getTotal())
+                .itens(converter(pedido.getItens()))
+                .build();
+    }
+
+    private List<InformacoesItemPedidoDTO> converter(List<ItemPedido> itens) {
+        if(CollectionUtils.isEmpty(itens)) return Collections.emptyList();
+
+        return itens.stream().map(
+                item -> InformacoesItemPedidoDTO
+                        .builder()
+                        .descricaoProduto(item.getProduto().getDescricao())
+                        .precoUnitario(item.getProduto().getPreco())
+                        .quantidade(item.getQuantidade())
+                        .build()
+        ).collect(Collectors.toList());
     }
 }
--- src/main/java/io/github/erichika/service/PedidoService.java
+++ src/main/java/io/github/erichika/service/PedidoService.java
@@ -3,6 +3,10 @@ package io.github.erichika.service;
 import io.github.erichika.domain.entity.Pedido;
 import io.github.erichika.rest.dto.PedidoDTO;
 
+import java.util.Optional;
+
 public interface PedidoService {
     Pedido salvar(PedidoDTO dto);
+
+    Optional<Pedido> obterPedidoCompleto(Integer id);
 }
