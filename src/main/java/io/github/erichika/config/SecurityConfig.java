cbec0d27975e1ac3022f9160c13731737e60fbfa
erich-ika <erich.sabe.tudo@gmail.com>
2023-04-06 17:06:42 -0300

    Não ta dando certo
    eu to tentando entender como que eu faço isso sozinho mas ta complicado... parei aqui. Dá erro na execução.

--- src/main/java/io/github/erichika/config/SecurityConfig.java
+++ src/main/java/io/github/erichika/config/SecurityConfig.java
@@ -1,17 +1,42 @@
 package io.github.erichika.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 
+@Component
 @EnableWebSecurity
 public class SecurityConfig {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
-        return web -> {
-        };
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected InMemoryUserDetailsManager userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User
+                        .withUsername("fulano")
+                        .password(passwordEncoder.encode("123"))
+                        .roles("USER")
+                        .build()
+        );
+        return manager;
     }
 
     @Bean
