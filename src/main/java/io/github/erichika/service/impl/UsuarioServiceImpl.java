db13ad3a5a9981568f00027492ae6057339f6518
erich-ika <erich.sabe.tudo@gmail.com>
2023-04-10 14:40:05 -0300

    Implementação do UserDetailsService

---
+++ src/main/java/io/github/erichika/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,30 @@
+package io.github.erichika.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (!username.equals("Erich")) {
+            throw new UsernameNotFoundException("Usuário não encontrado na base.");
+        }
+
+        return User
+                .builder()
+                .username("Erich")
+                .password(encoder.encode("123"))
+                .roles("USER", "ADMIN")
+                .build();
+    }
+}
