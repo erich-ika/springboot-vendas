98fa2f7f206737207a4e849e8019694f50cf7ba8
erich-ika <erich.sabe.tudo@gmail.com>
2023-04-10 14:02:41 -0300

    Configurando a autorização de URLS

--- src/main/java/io/github/erichika/config/SecurityConfig.java
+++ src/main/java/io/github/erichika/config/SecurityConfig.java
@@ -27,6 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests(auth -> {
+                    auth.antMatchers("/api/clientes/**").authenticated();
+                })
+                .formLogin();
     }
 }
