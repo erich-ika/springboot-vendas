d97033c2f77e2320a3013f7deb2bb02dc56fc0fe
erich-ika <erich.sabe.tudo@gmail.com>
2023-04-14 11:40:32 -0300

    Testando o repositório de Cidades

--- src/main/java/io/github/erichika/localizacao/LocalizacaoApplication.java
+++ src/main/java/io/github/erichika/localizacao/LocalizacaoApplication.java
@@ -1,14 +1,33 @@
 package io.github.erichika.localizacao;
 
+import io.github.erichika.localizacao.domain.entity.Cidade;
+import io.github.erichika.localizacao.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Inicializado!");
+	public void run(String... args) {
+		salvarCidade();
+		listarCidades();
+	}
+
+	@Transactional
+	void salvarCidade() {
+		var cidade = new Cidade(1L, "São Paulo", 12330000L);
+		cidadeRepository.save(cidade);
+	}
+
+	void listarCidades() {
+		cidadeRepository.findAll().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
